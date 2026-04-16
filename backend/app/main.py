729d0d68e78199516f4e663a83591c30adcb56fb
DevMaheshBatta <devmaheshbatta@gmail.com>
2026-04-16 12:38:15 +0530

    issue fixed

--- backend/app/main.py
+++ backend/app/main.py
@@ -29,7 +29,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://ai-rag-project-frontend-tjra.vercel.app/"],
+    allow_origins=["https://ai-rag-project-frontend-4ys42xe66-dev-mahesh-battas-projects.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
